eee6089efa9ee3a88a9fbd258eaec8a4851c5efe
DMIND408P <yyog6@student.monash.edu>
2018-10-29 12:35:38 +1100

    Added additional docstring to the methods in the Rule class

--- rules_engine/rules/rules.py
+++ rules_engine/rules/rules.py
@@ -1,11 +1,15 @@
 import abc
  class Rule(abc):
      def __init__(self, rule_id):
+        """
+         :param rule_id: Unique Rule Id
+        """
         self.rule_id = rule_id
      @abc.abstractmethod
     def execute(self, rule_input):
         """
          :param rule_input: The Input entity on which the Rule will be applied.
         :rtype: Will return boolean i.e True/False
+        :rtype: Should return a tuple i.e (boolean, str) eg: (True, "Pass")  or (False, "Failure reason")
         """
         pass
